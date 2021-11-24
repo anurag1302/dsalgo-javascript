54dac9e345adc963b569fca9b51f4b13b2f14f6a
Anurag Sinha <anurag3487@gmail.com>
2021-11-24 11:20:58 +0530

    reverse string using array reverse method

--- practice/reverse-string/index.js
+++ practice/reverse-string/index.js
@@ -1,3 +1,4 @@
+//using for loop
 function reverseString(str) {
   let result = "";
   for (let i = str.length - 1; i >= 0; i--) {
@@ -8,3 +9,13 @@ function reverseString(str) {
 
 console.log(reverseString("Anurag"));
 console.log(reverseString("Hello World !!!"));
+
+// 1. convert string to array
+// 2. apply reverse() method on the array
+// 3. convert the array back to string
+function reverseStringUsingReverse(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log(reverseStringUsingReverse("Anurag"));
+console.log(reverseStringUsingReverse("Hello World !!!"));
