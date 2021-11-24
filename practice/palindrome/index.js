7d5751f24b9e699dd6219dec186aa8e34df94965
Anurag Sinha <anurag3487@gmail.com>
2021-11-24 18:00:53 +0530

    palindrome - 2nd approach using every()

--- practice/palindrome/index.js
+++ practice/palindrome/index.js
@@ -1,22 +1,43 @@
 //Palindrome
 
-function palindrome(str) {
+// function palindrome(str) {
+//   if (!str) {
+//     return "Invalid Input";
+//   }
+//   let reversedStr = str
+//     .replace(" ", "")
+//     .split("")
+//     .reduce((rev, item) => item + rev, "");
+
+//   return str.replace(" ", "").toLowerCase() === reversedStr.toLowerCase();
+// }
+
+// console.log(palindrome("abba"));
+// console.log(palindrome("abcd"));
+// console.log(palindrome("racecar"));
+// console.log(palindrome("civic"));
+// console.log(palindrome("taco cat"));
+// console.log(palindrome("Aibohphobia"));
+// console.log(palindrome(null));
+// console.log(palindrome(""));
+
+function palindromeUsingEvery(str) {
   if (!str) {
     return "Invalid Input";
   }
-  let reversedStr = str
+  return str
     .replace(" ", "")
     .split("")
-    .reduce((rev, item) => item + rev, "");
-
-  return str.replace(" ", "").toLowerCase() === reversedStr.toLowerCase();
+    .every((item, index) => {
+      return item.toLowerCase() === str[str.length - index - 1].toLowerCase();
+    });
 }
 
-console.log(palindrome("abba"));
-console.log(palindrome("abcd"));
-console.log(palindrome("racecar"));
-console.log(palindrome("civic"));
-console.log(palindrome("taco cat"));
-console.log(palindrome("Aibohphobia"));
-console.log(palindrome(null));
-console.log(palindrome(""));
+console.log(palindromeUsingEvery("abba"));
+console.log(palindromeUsingEvery("abcd"));
+console.log(palindromeUsingEvery("racecar"));
+console.log(palindromeUsingEvery("civic"));
+console.log(palindromeUsingEvery("taco cat"));
+console.log(palindromeUsingEvery("Aibohphobia"));
+console.log(palindromeUsingEvery(null));
+console.log(palindromeUsingEvery(""));
