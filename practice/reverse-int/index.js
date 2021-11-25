02f0648d52ccb84c5967f7b656e314af4d3ff706
Anurag Sinha <anurag3487@gmail.com>
2021-11-25 14:40:16 +0530

    reverse int - tweaks

--- practice/reverse-int/index.js
+++ practice/reverse-int/index.js
@@ -1,25 +1,25 @@
 //Reverse Int
 
-// function reverseInt(number) {
-//   if (!number || isNaN(number)) {
-//     return "Invalid format - input is not a integer";
-//   }
-//   let strArray = number.toString().split("");
-//   let isNegative = strArray.indexOf("-") !== -1 ? true : false;
-//   let reversedStr = strArray.reverse().join("");
-//   reversedStr = isNegative ? `${"-"}${reversedStr}` : reversedStr;
-//   return parseInt(reversedStr);
-// }
+function reverseInt(number) {
+  if (!number || isNaN(number)) {
+    return "Invalid format - input is not a integer";
+  }
+  let strArray = number.toString().split("");
+  let isNegative = strArray.indexOf("-") !== -1 ? true : false;
+  let reversedStr = strArray.reverse().join("");
+  reversedStr = isNegative ? `${"-"}${reversedStr}` : reversedStr;
+  return parseInt(reversedStr);
+}
 
-// console.log(reverseInt(123));
-// console.log(reverseInt(-981));
-// console.log(reverseInt(500));
-// console.log(reverseInt(-900));
-// console.log(reverseInt(-900123));
-// console.log(reverseInt(""));
-// console.log(reverseInt("hello world"));
-// console.log(reverseInt(null));
-// console.log(reverseInt(NaN));
+console.log(reverseInt(123));
+console.log(reverseInt(-981));
+console.log(reverseInt(500));
+console.log(reverseInt(-900));
+console.log(reverseInt(-900123));
+console.log(reverseInt(""));
+console.log(reverseInt("hello world"));
+console.log(reverseInt(null));
+console.log(reverseInt(NaN));
 
 //Alternate approach using Math.sign() method
 function reverseIntAlternate(number) {
